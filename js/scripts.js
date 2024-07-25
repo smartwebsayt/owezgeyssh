ad8dd16aa8b6166f9b0746f874fe145fbde2455b
smartwebsayt <164928375+smartwebsayt@users.noreply.github.com>
2024-07-25 19:32:52 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - SB UI Kit Pro v2.0.5 (https://shop.startbootstrap.com/product/sb-ui-kit-pro)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under SEE_LICENSE (https://github.com/BlackrockDigital/sb-ui-kit-pro/blob/master/LICENSE)
+*/window.addEventListener('DOMContentLoaded',event=>{feather.replace();var tooltipTriggerList=[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));var tooltipList=tooltipTriggerList.map(function(tooltipTriggerEl){return new bootstrap.Tooltip(tooltipTriggerEl);});var popoverTriggerList=[].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));var popoverList=popoverTriggerList.map(function(popoverTriggerEl){return new bootstrap.Popover(popoverTriggerEl);});const navStick=document.body.querySelector('#navStick');if(navStick){new bootstrap.ScrollSpy(document.body,{target:'#navStick',offset:82,});}
+var navbarCollapse=function(){const navbarMarketingTransparentFixed=document.body.querySelector('.navbar-marketing.bg-transparent.fixed-top');if(!navbarMarketingTransparentFixed){return;}
+if(window.scrollY===0){navbarMarketingTransparentFixed.classList.remove('navbar-scrolled')}else{navbarMarketingTransparentFixed.classList.add('navbar-scrolled')}};navbarCollapse();document.addEventListener('scroll',navbarCollapse);});
